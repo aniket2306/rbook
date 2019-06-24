a610943200a5ece4f6e82a46100e4d61ff9b0917
Mayank <mayankchandra07@gmail.com>
2019-06-24 20:28:05 +0530

    fixed stuff

--- aud/aud/settings.py
+++ aud/aud/settings.py
@@ -85,13 +85,9 @@ WSGI_APPLICATION = 'aud.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'aud',
-        'USER': 'aud',
-        'PASSWORD': 'aud',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+}
 }
 
 
